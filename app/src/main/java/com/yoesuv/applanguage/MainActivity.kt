ab0370a8d0eca14148969c0204c6a335dbb806b6
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-06-07 08:40:59 +0700

    :tada: initial commit

---
+++ app/src/main/java/com/yoesuv/applanguage/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.yoesuv.applanguage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.yoesuv.applanguage.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbarMain)
+    }
+
+}
