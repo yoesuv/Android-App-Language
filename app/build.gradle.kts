de7882edb113d9f26c0139cc5e547251058b4192
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-12-28 07:04:41 +0700

    :arrow_up: upgrade gradle & target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.yoesuv.applanguage"
-    compileSdk  = 33
+    compileSdk  = 34
 
     defaultConfig {
         applicationId = "com.yoesuv.applanguage"
-        minSdk = 21
-        targetSdk = 33
+        minSdk = 24
+        targetSdk = 34
         versionCode = 1
         versionName  = "1.0.1"
 
