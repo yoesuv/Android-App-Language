a1d3e98c86408b4c62f0745e169b674d0f0d7ada
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-12-30 05:42:24 +0700

    :arrow_up: update viewmodel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 }
